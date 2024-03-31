14de650fc47f87f854609b9ba36fc70b0095a6d3
vladyslav-pro <rossomaha007@gmail.com>
2024-04-01 00:38:24 +0300

    initial commit

---
+++ cafe-fe/src/app/main-page/main-page.component.ts
@@ -0,0 +1,12 @@
+import {Component} from "@angular/core";
+
+@Component({
+  standalone: true,
+  selector: 'main-page',
+  templateUrl: '.main-page.component.html',
+  styleUrl: '.main-page.component.scss',
+  imports: []
+})
+export class MainPageComponent {
+
+}
